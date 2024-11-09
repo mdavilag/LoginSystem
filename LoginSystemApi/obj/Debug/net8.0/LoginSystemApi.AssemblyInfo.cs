a21a555d629d419606c392aa023c8a8719e72d32
Matheus Davila <mat.97@hotmail.com>
2024-11-09 18:40:06 -0300

    Add .gitignore

--- LoginSystemApi/obj/Debug/net8.0/LoginSystemApi.AssemblyInfo.cs
+++ LoginSystemApi/obj/Debug/net8.0/LoginSystemApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginSystemApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca04bd61706e7375e396a04824dffd7acdf28176")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginSystemApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginSystemApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
