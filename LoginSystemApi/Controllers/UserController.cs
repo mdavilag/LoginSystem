1e9a848caa5f0e8497c1f6b21228efb1d93dd6cc
Matheus Davila <mat.97@hotmail.com>
2024-11-11 14:45:03 -0300

    Create User GetById Method

--- LoginSystemApi/Controllers/UserController.cs
+++ LoginSystemApi/Controllers/UserController.cs
@@ -1,11 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LoginSystemApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
 
 namespace LoginSystemApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]/[action]")]
     public class UserController : ControllerBase
     {
-        
+        private readonly DataContext _context;
+
+        public UserController(DataContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(_context.Users.ToList());
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (user != null) return Ok(user);
+
+            else return NotFound();
+
+        }
     }
 }
