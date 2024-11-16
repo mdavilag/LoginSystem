1fc2070683acaf55ae715200704677a99dc042b0
Matheus Davila <mat.97@hotmail.com>
2024-11-16 11:35:00 -0300

    Create ResultViewModel

--- LoginSystemApi/ViewModels/ResultViewModel.cs
+++ LoginSystemApi/ViewModels/ResultViewModel.cs
@@ -1,6 +1,27 @@
 ﻿namespace LoginSystemApi.ViewModels
 {
-    public class ResultViewModel
+    public class ResultViewModel<T>
     {
+        public T? Data { get; set; }
+        public List<string> Errors { get; set; } = new List<string>();
+
+        public ResultViewModel(T data, List<string> errors)
+        {
+            Data = data;
+            Errors = errors;
+        }
+        public ResultViewModel(T data)
+        {
+            Data= data;
+        }
+        public ResultViewModel(List<string> errors)
+        {
+            Errors= errors;
+        }
+        public ResultViewModel(string error)
+        {
+            Errors.Add(error);
+        }
+
     }
 }
